7d70115304152e7c5b07a799056f98ecb243c0ac
Hrod3879 <Hrod3879@users.noreply.github.com>
2023-10-02 17:27:58 -0400

    10/2

---
+++ Lists, Inputs, Print, Variables.py
@@ -0,0 +1,13 @@
+#P.1
+wordbank= ["indentation", "spaces"] 
+#P.2
+tlgstudents= ['Albert', 'Anthony', 'Brenden', 'Craig', 'Deja', 'Elihu', 'Eric', 'Giovanni', 'James', 'Joshua', 'Maria', 'Mohamed', 'PJ', 'Philip', 'Sagan', 'Suchit', 'Meka', 'Trey', 'Winton', 'Xiuxiang', 'Yaping']
+#P.3
+wordbank.append(4)
+#P.4
+num = int(input("Pick a number between 1 and 20: "))
+#P.5
+choice = int(input("Pick a number!! "))
+student_name = tlgstudents[choice]
+#P.6
+print(f"{student_name} always uses {wordbank[2]} {wordbank[1]} to indent.")
